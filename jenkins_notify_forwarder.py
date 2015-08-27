ec822d1936445008875bf1ddb6242f377831eca0
Chris Scutcher <cscutche@cisco.com>
2015-08-27 19:01:23 +0100

    Initial commit

---
+++ jenkins_notify_forwarder.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Forwards jenkins scm notifications to multiple jenkins instances
+"""
+import logging
+from flask import Flask
+
+DEV_LOGGER = logging.getLogger(__name__)
+
+
+app = Flask(__name__)
+
+@app.route('/git/notifyCommit')
+def recieve_git_notification(request):
+    '''
+    Receive notification and forward onto downstream servers
+    '''
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Setup for ttm_cli
+"""
+from setuptools import setup, find_packages
+setup(
+    name='jenkins_notify_forwarder',
+    description='Forwards jenkins scm notifications to multiple jenkins instances',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+    ],
+    entry_points={
+    },
+    author='Chris Scutcher',
+    author_email='cscutche@cisco.com',
+    url='https://stash-eng-gpk1.cisco.com/stash/projects/EXP/repos/ttm_cli',
+)
